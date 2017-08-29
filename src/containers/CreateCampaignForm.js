7f87dd551611ff15989a4f0124123b80b34f138e
Lukeghenco <lukeghenco@gmail.com>
2017-08-29 14:30:56 -0400

    Add Campaigns and CreateCampaignForm containers

---
+++ src/containers/CreateCampaignForm.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class CreateCampaignForm extends Component {
+
+    render() {
+        return (
+            <form></form>
+        );
+    }
+};
+
+export default CreateCampaignForm;
