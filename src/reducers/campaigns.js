5bd3349ab275a157369ec1a67e9218e65e2c403d
Lukeghenco <lukeghenco@gmail.com>
2017-08-29 07:53:42 -0400

    Add campaigns reducer

---
+++ src/reducers/campaigns.js
@@ -0,0 +1,35 @@
+export default (state = [], action) => {
+    switch(action.type) {
+
+        case 'SET_CAMPAIGNS': {
+            return action.campaigns;
+        };
+
+        case 'ADD_CAMPAIGN': {
+            return state.concat(action.campaign);
+        };
+
+        case 'REPLACE_CAMPAIGN': {
+            const index = state.findIndex(campaign => campaign.id === action.campaign.id);
+            
+            return [
+                ...state.slice(0, index),
+                action.campaign,
+                ...state.slice(index + 1)
+            ];
+        };
+
+        case 'REMOVE_CAMPAIGN': {
+            const index = state.findIndex(campaign => campaign.id === action.campaignId);
+            
+            return [
+                ...state.slice(0, index),
+                ...state.slice(index + 1)
+            ];
+        }
+        
+        default: {
+            return state;
+        };
+    };
+};
