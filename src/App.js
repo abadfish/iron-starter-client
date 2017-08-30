cfe9802a01019473ceb75c869dd712d0b16c297b
Lukeghenco <lukeghenco@gmail.com>
2017-08-30 10:16:20 -0400

    Update App.js components Links and Routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
 import Campaigns from './containers/Campaigns';
-import CreateCampaignForm from './containers/CreateCampaignForm';
 
 class App extends Component {
     render() {
+
+        const navLinkStyle = { 
+            marginRight: '12px',
+         };
+        const headerStyle = { 
+            width: '100%',
+            borderBottom: 'solid 1px black',
+            textAlign: 'center',
+        };
+        
         return (
             <Router>
                 <div>
-                    <h1>Iron Starter</h1>
-                    <Link id="createCampaignLink" to="/">Campaigns</Link>
-                    <Link id="createCampaignLink" to="/campaigns/new">Create Campaign</Link>
-                    <Route exact path="/" component={Campaigns} />
-                    <Route path="/campaigns/new" component={CreateCampaignForm} />
+                    <h1 className="header" style={headerStyle}>
+                        <Link to="/">Iron Starter</Link>
+                    </h1>
+                    <div className="navbar">
+                        <Link style={navLinkStyle} to="/campaigns">Campaigns</Link>
+                        <Link style={navLinkStyle} to="/campaigns/new">Create Campaign</Link>
+                    </div>
+                    <div className="mainRoutes">
+                        <Route exact path="/" component={Home} />
+                        <Route path="/campaigns" component={Campaigns} />
+                    </div>
                 </div>
             </Router>
         );
