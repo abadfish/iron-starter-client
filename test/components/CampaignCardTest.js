4375246aaf417083cbb2ff39c47824db19479c49
Luke Ghenco <lukeghenco@gmail.com>
2017-09-01 09:27:15 -0400

    Move CampaignCard wrapper to be set before each test

--- test/components/CampaignCardTest.js
+++ test/components/CampaignCardTest.js
@@ -1,15 +1,18 @@
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
-import React from 'react'
+import React from 'react';
+import CampaignCard from '../../src/components/CampaignCard';
 
-import CampaignCard from '../../src/components/CampaignCard'
-
-describe('CampaginCard', () => {
-  const wrapper = shallow(<CampaignCard campaign={{title: 'Throw me a Party'}}/>)
+describe('CampaignCard', () => {
+  const wrapper;
+  
+  beforeEach(() => {
+    wrapper = shallow(<CampaignCard campaign={{ title: 'Throw me a Party' }} />);
+  });
 
   it('renders the title of the campaign', () => {
-    const header = wrapper.find('h2')
-    expect(header.length).to.eq(1, "The campaign card isn't rendering exactly one h2")
-    expect(header.text()).to.eq('Throw me a Party', "The Campaign Card isn't rendering the title of the campaign")
-  })
-})
+    const header = wrapper.find('h2');
+    expect(header.length).to.eq(1, "The campaign card isn't rendering exactly one h2");
+    expect(header.text()).to.eq('Throw me a Party', "The Campaign Card isn't rendering the title of the campaign");
+  });
+});
