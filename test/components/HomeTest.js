00ac51017d4e2f0840f3cbd9e1f4f783017d7886
Luke Ghenco <lukeghenco@gmail.com>
2017-09-01 09:27:58 -0400

    Set Home wrapper to be set before each test

--- test/components/HomeTest.js
+++ test/components/HomeTest.js
@@ -5,10 +5,15 @@ import React from 'react'
 import Home from '../../src/components/Home'
 
 describe('Home', () => {
-  const wrapper = shallow(<Home />)
+  let wrapper; 
+  
+  beforeEach(() => {
+    wrapper = shallow(<Home />);
+  });
+    
   it('renders a link to "/campaigns"', () => {
-    const link = wrapper.find('Link')
-    expect(link.length).to.eq(1, "The Home component isn't rendering any Links")
-    expect(link.node.props.to).to.eq('/campaigns', "The Link in the Home Component doesn't go to '/campaigns'")
-  })
-})
+    const link = wrapper.find('Link');
+    expect(link.length).to.eq(1, "The Home component isn't rendering any Links");
+    expect(link.node.props.to).to.eq('/campaigns', "The Link in the Home Component doesn't go to '/campaigns'");
+  });
+});
